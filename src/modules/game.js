8af4569eac9acd6ec1dcdcd6ff52e84666a8640a
ayan4m1 <andrew@bulletlogic.com>
2022-06-11 12:21:35 -0400

    cause mid-game stop to sync db properly

--- src/modules/game.js
+++ src/modules/game.js
@@ -98,6 +98,10 @@ const createGame = () =>
             GAME_END: {
               target: 'stopGame'
             },
+            GAME_STOP: {
+              target: 'stopGame',
+              actions: 'notifyGameStop'
+            },
             CARD_PLAY: [
               {
                 actions: 'notifyInvalidPlayer',
